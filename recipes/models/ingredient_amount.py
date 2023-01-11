d146a737a859afb60e5c7816d318dab9518dd7c3
jacob ostapenko <jacobostapenko10@gmail.com>
2023-01-10 22:52:13 -0500

    adding field to ingredientAmount and migrating updating data to db

--- recipes/models/ingredient_amount.py
+++ recipes/models/ingredient_amount.py
@@ -6,8 +6,10 @@ from recipes.models import Recipe, Ingredient
 class IngredientAmount(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
-
+    def __str__(self):
+        return str(self.ingredient) + ' ' + str(self.amount)
 
     class Meta:
         db_table = 'ingredientamount'
