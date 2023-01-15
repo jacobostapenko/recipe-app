8592925a325e5c6940748e4beb69c63a3412b355
Dmitri <--global>
2023-01-15 15:14:15 -0700

    update recipes to link out

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='recipes-home'),
     path('about/', views.about, name='recipes-about'),
+    #TODO: delete
     path('SpecificRecipe/', views.recipe, name='recipe')
 
 ]
